e2fc39fb1fce5693705a4ad5b68d5f90c85a8001
Potato <po7a7o@me.com>
2018-05-28 12:40:02 -0300

    .

--- ML002.py
+++ ML002.py
@@ -1,2 +1,15 @@
-import os
-import pandas
+# graficos embebidos
+%matplotlib inline
+
+# importando pandas, numpy y matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importando los datasets de sklearn
+from sklearn import datasets
+
+boston = datasets.load_boston()
+boston_df = pd.DataFrame(boston.data, columns=boston.feature_names)
+boston_df['TARGET'] = boston.target
+boston_df.head() # estructura de nuestro dataset.
