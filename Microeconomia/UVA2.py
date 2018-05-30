5e97812ea62b092692a862e552565cb5718ca3db
Potato <po7a7o@me.com>
2018-05-29 21:16:34 -0300

    .

--- Microeconomia/UVA2.py
+++ Microeconomia/UVA2.py
@@ -5,30 +5,116 @@ import sympy as sy
 import numpy as np
 import matplotlib as plt
 
-x = Symbol('x')
+# Etiquetas del grafico
+x_label = "Q"
+y_label = "P"
+
+# Definir que x es un simbolo
+# x = Symbol('x')
 
 #Qd = input("Ingrese función de demanda: /n")
 #Qo = input("Ingrese función de oferta: /n")
 
-Qd = 100-15*x
-Qo = 50+10*x
+# Definir las funciones
+def Qd(x):
+#    return 5-0.1*x
+    return 100-15*x
+def Qo(x):
+#    return -4+0.2*x
+    return 50+10*x
 
-print ("Función de demanda: ",Qd)
-print ("Función de oferta: ",Qo)
+temp1 = Qd(0)
+temp2 = Qo(0)
+temp3a = Qd(x)
+temp3 = solve(temp3a)
 
-x_graph = range(-10, 15)
+# Nuevas ecuaciones
+temp3_Qd = Qd(x)
+temp_b_Qd = solve(temp3_Qd)
+b_Qd = int(temp_b_Qd[0])
+m_Qd = -0.1*x
+# print (b_Qd[0],m_Qd)
+# print (temp_b_Qd)
+
+def nQd(x):
+    return b_Qd + m_Qd
+
+# print (nQd(x))
+
+temp3_Qo = Qo(x)
+temp_b_Qo = solve(temp3_Qo)
+b_Qo = temp_b_Qo[0]
+m_Qo = +0.2*x
+print (b_Qd,m_Qd)
+print (temp_b_Qd)
+asa = b_Qo+m_Qo
+
+def nQo(x):
+    return asa
+    return 0.2*x -5
+
+print (asa)
+print (Qo(1))
+print (nQo(1))
+
+print ("Función de oferta       : ",Qo(x),"//",nQo(1))
+
+# Calcular Precio Equilibrio
+q = Qd(x)+-1*(Qo(x))
+# Valor P*
+p_equil_temp = solve(q)
+p_equil = p_equil_temp[0]
+# Valor Q*
+q_equil = Qd(p_equil)
+
+# Imprimir las funciones
+print ("Función de demanda      : ",Qd(x),"//",nQd(1))
+print ("Función de oferta       : ",Qo(x),"//",nQo(1))
+
+# Valor x, que en nuestro caso es P
+print ("Calcular punto de equilibrio, igualar Qd y Qo")
+print (Qd(x)," = ",Qo(x))
+print ("Ecuacion simplificada   : ",q)
+print ("Precio Equilibrio (P*)  : ",p_equil)
+print ("Cantidad Equilibrio (Q*): ",q_equil)
+
+# Calcular los limites del grafico
+x_lim = Qo(0)+10
+y_lim = Qd(0)+10
+
+print (x_lim,y_lim)
+
+x_graph = range(0, 110)
 
 # Graficar ambas funciones.
-pyplot.plot(x, [Qd(i) for i in x])
-pyplot.plot(x, [Qo(i) for i in x])
+pyplot.plot(x_graph, [nQd(i) for i in x_graph])
+pyplot.plot(x_graph, [nQo(i) for i in x_graph])
 
 # Establecer el color de los ejes.
 pyplot.axhline(0, color="black")
 pyplot.axvline(0, color="black")
 
 # Limitar los valores de los ejes.
-pyplot.xlim(-10, 10)
-pyplot.ylim(-10, 10)
+#pyplot.xlim(0, x_lim)
+#pyplot.ylim(0, y_lim)
+pyplot.xlim(0, 110)
+pyplot.ylim(0, 10)
+
+# Definir titulo del grafico
+pyplot.title("Oferta y Demanda")
+
+# Definir etiquetas ejes
+pyplot.xlabel(x_label)
+pyplot.ylabel(y_label)
+
+# Agregar leyenda al grafico
+pyplot.legend(("Demanda","Oferta"), loc="upper right")
+
+# Agregar P*,Q*
+pyplot.text(p_equil,q_equil,"Punto de equilibrio")
+
+# Agregar etiqueta al grafico
+# pyplot.text(5,10,"punto")
 
 # Guardar gráfico como imágen PNG.
 #pyplot.savefig("output.png")
--- Microeconomia/test.py
+++ Microeconomia/test.py
@@ -1,8 +1,37 @@
 from sympy import *
-import sympy as sy
+from matplotlib import pyplot
 
-x = Symbol('x')
-#fx = (2*x**3-4*x)
-fx = input("Ingrese funcion: /n")
-dx = diff(fx)
-print (dx)
+import sympy as sy
+import numpy as np
+import matplotlib as plt
+
+
+#Función Lineal.
+def Qd(x):
+    return asa
+
+print ("aa: ", Qd(x))
+
+#    return 5-0.1*x
+#En esta variable se genera una lista con valores del -10 al 10.
+#Todos estos valores serán los que tomara x.
+x = range(-100, 100)
+
+#Con el método plot especificamos que función graficaremos.
+#El primer argumento es la variable con los valores de x.
+#El segundo argumento le pasamos todos estos valares a la función con ayuda de un bucle.
+pyplot.plot(x, [f(i) for i in x])
+
+#Establecemos el color de los ejes.
+pyplot.axhline(0, color="black")
+pyplot.axvline(0, color="black")
+
+#Especificamos los limites de los ejes.
+pyplot.xlim(-5, 60)
+pyplot.ylim(-5, 6)
+
+#Guardamos el grafico en una imagen "png".
+#pyplot.savefig("función_lineal.png")
+
+# Mostramos el gráfico.
+pyplot.show()
